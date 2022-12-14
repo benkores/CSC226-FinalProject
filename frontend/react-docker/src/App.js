0b85187bb6b725b769c47dccabc68e66b10a8071
benkores <benkores@gmail.com>
2022-12-14 00:01:37 -0500

    remove uneeded components from App

--- frontend/react-docker/src/App.js
+++ frontend/react-docker/src/App.js
@@ -1,7 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 import MyComponent from "./components/MyComponent.js";
-import {Autocomplete, Button, Checkbox, Slider, Stack, TextField} from "@mui/material";
 
 function App() {
   return (
@@ -10,8 +8,10 @@ function App() {
         <img src={require('./imgs/news_icon.png')} className="App-logo" alt="logo" />
         <h2>Live News from 7,500 News Sources Worldwide!</h2>
       </header>
-        <h2>Enter a country, news source, and category to get started</h2>
+        <h2>Enter a country, news source, and category below to get started</h2>
     </div>
   );
 }
+
+
 export default App;
