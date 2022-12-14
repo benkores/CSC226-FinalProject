913af959023b8abb17a88df10aa4361cb9cf0513
benkores <benkores@gmail.com>
2022-12-13 22:50:25 -0500

    app header

--- frontend/react-docker/src/App.js
+++ frontend/react-docker/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import MyComponent from "./components/MyComponent";
+import MyComponent from "./components/MyComponent.js";
 import {Autocomplete, Button, Checkbox, Slider, Stack, TextField} from "@mui/material";
 
 function App() {
@@ -10,6 +10,7 @@ function App() {
         <img src={require('./imgs/news_icon.png')} className="App-logo" alt="logo" />
         <h2>Live News from 7,500 News Sources Worldwide!</h2>
       </header>
+        <h2>Enter a country, news source, and category to get started</h2>
     </div>
   );
 }
